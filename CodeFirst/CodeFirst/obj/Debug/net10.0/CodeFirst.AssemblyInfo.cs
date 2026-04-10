52bcd3aeb72fe1446fc6e25777e93b4c9e5fadb6
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-04-10 23:02:44 +0700

    CRUD controllers and view for Category and Product

--- CodeFirst/CodeFirst/obj/Debug/net10.0/CodeFirst.AssemblyInfo.cs
+++ CodeFirst/CodeFirst/obj/Debug/net10.0/CodeFirst.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cca78a6f5d45c7968c704a4e9606cf49bc0b308")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a765860a07c7077c74819c8dadd809c222fbefd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
